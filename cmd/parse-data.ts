0ae5f3f2e1278df5c7f84100e237d6901113fc46
Edward Tanguay (windows) <edward@tanguay.info>
2026-08-28 20:00:49 +0200

    first-customize-001

--- cmd/parse-data.ts
+++ cmd/parse-data.ts
@@ -1,12 +1,9 @@
-import { parseSeeAlsoLinks } from "./parse-data/parse-see-also-links.ts";
 import { parseNotes } from "./parse-data/parse-notes.ts";
 
 function main(): void {
   console.log("==> Running data parsing pipeline...");
-  parseSeeAlsoLinks();
   parseNotes();
   console.log("==> Data parsing completed successfully.");
 }
 
 main();
-
